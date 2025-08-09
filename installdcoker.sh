3ab5cf691fe8877108e1153c8bdf9b59aa53b26a
kamandanuX <radenkamandanu64@gmail.com>
2025-08-10 01:36:58 +0700

    Create installdcoker.sh

---
+++ installdcoker.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+echo "========================="
+echo "INSTALL DOCKER"
+echo "========================="
+
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+sudo apt-get update
+
+sudo apt-get install ca-certificates curl
+
+sudo install -m 0755 -d /etc/apt/keyrings
+
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+sudo docker run hello-world
+
+echo "========================="
+echo "INSTALL CMS"
+echo "========================="
+
+sudo curl -o cms_install.sh https://cms.s.cdatayun.com/cms_linux/cms_install.sh && sudo chmod +x ./cms_install.sh
+
+sudo ./cms_install.sh install --version 3.6.9
